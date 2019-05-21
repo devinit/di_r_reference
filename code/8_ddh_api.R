9b6d8f11de5eb9c443fadf1482bd0578a85b3bf5
Alex Miller <alex.k.miller@gmail.com>
2019-05-21 13:19:13 -0400

    DDW Analyst UI and readme addition

--- code/8_ddh_api.R
+++ code/8_ddh_api.R
@@ -1,7 +1,9 @@
-list.of.packages <- c("data.table","reshape2")
+list.of.packages <- c("data.table","reshape2","httr","readr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only=T)
+
+#### OLD API INSTRUCTIONS ####
 
 # The DDH API is located here: http://212.111.41.68:8000/
 
@@ -55,3 +57,47 @@ pop_gov_rev$budget_type = NULL
 pop_gov_rev.m = melt(pop_gov_rev,id.vars=c("di_id","name","year","indicator"))
 pop_gov_rev.w = dcast(pop_gov_rev.m, di_id+name~indicator+year+variable)
 View(pop_gov_rev.w)
+
+#### NEW DDW UI INSTRUCTIONS ####
+
+# Change these to match your username and password from the DDW Analyst UI. Do not commit them to Github!
+ddw_user = "user"
+ddw_pass = "pass"
+
+# The DDW Analyst UI is build on a REST API that uses secure authorization tokens
+# First we need to POST the username and password to the service to generate a token
+base_url = "http://ddw.devinit.org/"
+token_response = POST(
+  paste0(base_url,"api/auth/login/"),
+  authenticate(ddw_user, ddw_pass)
+)
+if(token_response$status_code==200){
+  token = content(token_response)$token # Collect the token from the response for future queries
+}else{
+  message("Something went wrong with logging in!")
+}
+
+# Now that we've logged in, we can use the login response to see a list of queries associated with our account
+
+queries = content(token_response)$user$operation_set
+
+# Check if you've made any queries, otherwise default to a public one
+if(length(queries)>0){
+  first_query_id = queries[[1]]$id
+}else{
+  first_query_id = 262
+}
+
+# And once we've got a specific query, we can export the data
+
+query_data_response = POST(
+  paste0(base_url, "api/export/", first_query_id, "/"),
+  body=list("token"=token),
+  encode="form"
+)
+
+if(query_data_response$status_code==200){
+  query_data = content(query_data_response)
+}else{
+  message("Something went wrong with loading the data!")
+}
